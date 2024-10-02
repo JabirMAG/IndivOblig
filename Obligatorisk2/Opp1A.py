08ac1ce7fb0918ab1b6427ffa9e4ef5aa0948275
jabirmag <jabirgitale@gmail.com>
2024-10-02 23:23:42 +0200

    Endring på Oppgave1A

--- Obligatorisk2/Opp1A.py
+++ Obligatorisk2/Opp1A.py
@@ -1,23 +1,27 @@
 # Oppgave 1A 
 
-# Denne lager listen vi vil bruke til my-string-length
-Lengde = [5, 6, 7, 8, 1]
-
 # string_length(liste) :: liste -> Int
 # Input Vi putter inn listen vi vil bruke :: Liste
 # Output :: Funksjonen gir oss hvor mange string det er i listen i hele tall :: Int
-def string_length(liste): # String_length er funksjonen for å telle listen, (Liste) er input
-    """ Denne funksjonen teller opp hvor mange String det er i en liste, og gir en
-returverdi som viser dette. 
-"""
-    len(liste) # Mye simplere en pyret, Len vil da telle listen for oss
+def my_str_len(liste): # String_length er funksjonen for å telle listen, (Liste) er input
+    siffer = 0
+    for string in liste:
+        for char in string:
+            siffer += 1
+    return siffer
 
-print(len(Lengde)) # Print av "Lengde" Listen
+Lengde = ["Hello_World", "Goodbye_World", "Wonderful_World"] # Listen vi bruker for funksjonen
 
+print(my_str_len(Lengde)) # Printer ut funksjonen
+
+ 
+ 
+
+Tall = [1, 3, 4, 10, 5, 7]
 
 # Denne delen er for My-Max
 
 # max(list) :: liste -> int
 # Input :: Vi putter inn listen vi vil bruke :: Liste
 # Output :: Returverdi er det høyeste tallet i listen :: Int
-print(max(Lengde)) # Istedenfor en funksjon som på pyret så kan man bruke max(liste) i python
+print(max(Tall)) # Istedenfor en funksjon som på pyret så kan man bruke max(liste) i python
